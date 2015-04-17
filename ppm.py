4ab9291b694c422594a0e338c257a21915e8e324
Arne Kuederle <a.kuederle@gmail.com>
2015-04-17 13:57:03 +0200

    Modifikations and bugfixes for the wiw function
    1. modified the current figure default, to really get the most recent figure
    2. Always add label and visibility property, if they are not selcted by th user
    3. Added a option that the function just returns the data instead of displaying
    4. The table now displays all lines of a figure and not seperated by the axis

--- ppm.py
+++ ppm.py
@@ -8,27 +8,36 @@ Created on Fri Apr 10 12:54:34 2015
 import matplotlib.pyplot as plt
 import numpy as np
 
-def _print_table(row_header, line_header, data):
+
+def _print_table(row_header, data):
     """Controll table formatting of output.
     
     Code taken from: http://stackoverflow.com/questions/9535954/python-printing-lists-as-tabular-data
     """
-    row_format = "{:>15}" * (len(row_header) + 1)
+    line_numbers = np.arange(len(data))
+    row_format = "{}" + "{:>15}" * (len(row_header))
     print(row_format.format("", *row_header))
-    for line_head, row in zip(line_header, data):
-        print(row_format.format(line_head, *row))
+    for line_number, line in zip(line_numbers, data):
+        print(row_format.format(line_number, *line))
     
 
-def wiw(fig=plt.gcf(),props=["c", "marker"]):
-    """ 'What is what' display all lines with respective properties linked to a given figure. """
+def wiw(fig="_current_", props=["c", "marker"], _return=False):
+    """ Display all lines with respective properties linked to a given figure. """
+    if fig == "_current_":
+        fig = plt.gcf()
+    if "label" not in props and "visible" not in props: 
+        props[:0] = ["label", "visible"]
+    data = []
+    lines = []
     for ax in fig.get_axes():
-        lines = []
-        data = []
-        for line in ax.get_lines():
-            lines.append(plt.getp(line, "label"))
+        ax_lines = ax.get_lines()
+        lines.extend(ax_lines)
+        for line in ax_lines:
             data_line = []
             for prop in props:
                 data_line.append(plt.getp(line, prop))
             data.append(data_line)
-        _print_table(props, lines, data)
-        
+    if _return is True:
+        return lines, data
+    else:
+        _print_table(props, data)
