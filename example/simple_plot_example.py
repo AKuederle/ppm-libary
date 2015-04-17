a6228963210b35556086716a652f40343fd709aa
Arne Kuederle <a.kuederle@gmail.com>
2015-04-17 13:51:03 +0200

    modified the simple_plot_example to spawn a new figure on execution

--- example/simple_plot_example.py
+++ example/simple_plot_example.py
@@ -4,11 +4,12 @@ Created on Sat Apr 11 18:37:50 2015
 
 @author: Arne
 """
+import numpy as np
+import matplotlib.pyplot as plt
 
-from ppm import *
-
+plt.figure()
 x=np.arange(0,10,1)
 plt.plot(x,x,"o")        
 plt.plot(x,2*x,"o")
 plt.plot(x,3*x,"o")
-plt.plot(x,4*x,"o")
+plt.plot(x,4*x,"-")
